4a78e4e045d87064fd8373f0f7057b2dd01c2b6d
Paul Tarvydas <guitarvydas@gmail.com>
2021-08-31 18:34:03 -0400

    stubs for sdl2

---
+++ stubs.rkt
@@ -0,0 +1,36 @@
+#lang racket/gui
+
+(provide stub-halt-music stub-play-channel stub-load-mus stub-clear-error! stub-fade-out-music stub-play-music stub-get-error
+         stub-set-main-ready! stub-init! stub-open-audio! stub-load-wav stub-close-audio!
+         stub-default-format stub-default-channels
+         stub-sdl2-init!)
+
+(define (stub-halt-music)
+  #f)
+
+(define (stub-play-channel a b c) #f)
+
+(define (stub-load-mus x) #f)
+
+(define (stub-clear-error!) #f)
+
+(define (stub-fade-out-music x) #f)
+
+(define (stub-play-music x y) #f)
+
+(define (stub-open-audio! w x y z) #f)
+
+(define (stub-get-error) #f)
+
+(define (stub-set-main-ready!) #f)
+
+(define (stub-sdl2-init! x) #f)
+
+(define (stub-init! x) #f)
+
+(define stub-default-format #f)
+(define stub-default-channels #f)
+
+(define (stub-load-wav x) #f)
+
+(define (stub-close-audio!) #f)
