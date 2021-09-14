f3b0657db295657c092c079b21ce5d35aa924ceb
Paul Tarvydas <guitarvydas@gmail.com>
2021-09-14 15:05:36 -0400

    case using + instead of explicit recursion

--- excase-peg.rkt
+++ excase-peg.rkt
@@ -2,12 +2,12 @@
 (require "../racket-peg/s-exp.rkt");
 (define (compile-snippet e) (eval e));
 (define (ptest k)
-  (let ((pexpr (peg pcasetest "1 'a $ 2 'b $ 3 'c $ 4 'd $ else 'e")))
+  (let ((pexpr (peg pcasetest "$ 1 'a $ 2 'b $ 3 'c $ 4 'd $ else 'f")))
+    (println pexpr)
     (let ((e (compile-snippet pexpr)))
       (println (e k)))));
 
-pclause <- caselabel:s-exp _ e:s-exp -> (list (list caselabel) e);
-pelseclause <- 'else' _ (e:s-exp) -> (list 'else e);
-poneclause <- pelseclause / pclause;
-pclauses <- cl1:poneclause (_ '$' _ cl2:pclauses)? -> (if cl2 (cons cl1 cl2) (list cl1));
+pclause <- '$' _ (! 'else') caselabel:s-exp _ e:s-exp _ -> (list (list caselabel) e);
+pelseclause <- '$' _ 'else' _ e:s-exp -> (list 'else e);
+pclauses <- cl1:pclause+ ecl:pelseclause? -> (append cl1 (list ecl));
 pcasetest <- cls:pclauses -> (list 'lambda '(k) (cons 'case (cons 'k cls)));
